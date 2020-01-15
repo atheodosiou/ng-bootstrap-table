0712c15762fe734728270e63046bab226b10d538
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2020-01-15 18:06:17 +0200

    Rich Paginator

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -8,8 +8,20 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PaginatorComponent implements OnInit {
 
   constructor() { }
-  @Input() total:number=10;
+  _activePage:number=0;
+  _pages:any[]=[];
+  @Input() set total(value:number){
+    for(let i=0; i<value; i++){
+      let item={number:i+1};
+      this._pages.push(item);
+    }
+  }
   ngOnInit() {
   }
 
+  setPage(page:number, event){
+    event.preventDefault();
+    this._activePage=page;
+    console.log(this._activePage)
+  }
 }
