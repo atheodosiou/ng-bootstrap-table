ba9443846fc814e986707f5a4254727898a9f5ba
atheodosiou <anastasios.theodosiou@gmail.com>
2020-01-19 01:17:27 +0200

    Export table-columns.interface.ts and added in lib project

--- projects/ng-bootstrap-table/src/public-api.ts
+++ projects/ng-bootstrap-table/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ng-bootstrap-table
  */
 
-export * from './lib/ng-bootstrap-table.service';
-export * from './lib/ng-bootstrap-table.component';
+// export * from './lib/ng-bootstrap-table.service';
+// export * from './lib/ng-bootstrap-table.component';
 export * from './lib/ng-bootstrap-table.module';
+export * from './lib/table-columns.interface';
